92c6b0e74f9b8eabf54d3fe1dee26b6a901511c1
Lucian <lucian.jakob@stud.fh-rosenheim.de>
2018-07-17 12:40:37 +0200

    Add patch/todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 //local lib
 var {mongoose} = require('./db/mongoose');
@@ -72,7 +73,35 @@ if (!todo) {
 });
 
 
-//Get Methode
+app.patch('/todos/:id', (req, res) => {
+
+var id = req.params.id;
+var body = _.pick(req.body, ['text', 'completed']);
+
+if (!ObjectID.isValid(id)) {
+  return res.status(404).send();
+}
+
+if (_.isBoolean(body.completed) && body.completed) {
+  body.completedAt = new Date().getTime();
+}else {
+  body.completed = false;
+  body.completedAt = null;
+}
+
+Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+if (!todo) {
+  return res.status(404).send();
+}
+
+res.status(200).send({todo});
+
+}).catch((e) => {
+
+  res.status(400).send();
+});
+
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
